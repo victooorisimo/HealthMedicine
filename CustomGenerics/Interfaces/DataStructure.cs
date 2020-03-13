6f6e1fc94e9de4f96d2097523230a621199116e1
victooorisimo <vmelendrez2013241@gmail.com>
2020-03-13 16:26:22 -0600

    First commit

---
+++ CustomGenerics/Interfaces/DataStructure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * @author: Victor Noe Hernández
+ * @version: 1.0.0
+ * @description: class for NonlinearDataStructure
+ */
+
+namespace CustomGenerics.Interfaces {
+    public abstract class DataStructure<T> {
+        // Class methods
+        protected abstract void InsertValue(T value, Comparison<T> comparison);
+        protected abstract T DeleteValue(T value, Comparison<T> comparison);
+
+    }
+}
---
+++ CustomGenerics/Structures/AVLStructure.cs
@@ -0,0 +1,68 @@
+﻿using CustomGenerics.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenerics.Structures{
+
+    public class AVLStructure<T> : DataStructure<T>, IEnumerable<T>{
+
+        private Node<T> root;
+        private int size;
+
+        public void addElement(T newElement, Comparison<T> comparison){
+            InsertValue(newElement, comparison);
+        }
+
+        public void deleteElement(T deleteElement, Comparison<T> comparison){
+            DeleteValue(deleteElement, comparison);
+        }
+
+        public IEnumerator<T> GetEnumerator(){
+            throw new NotImplementedException();
+        }
+
+        protected override T DeleteValue(T value, Comparison<T> comparison){
+            if (!contains(root, value, comparison)){
+                return default(T);
+            }
+            root = root.rotate(root.removeElement(root, value, comparison));
+            size--;
+            return value;
+        }
+
+        private bool contains(Node<T> root, T value, Comparison<T> comparison){
+            if (root == null) return false;
+            if (comparison.Invoke(root.getValue(), value) == 0){
+                return true;
+            }else{
+                if (contains(root.getRigthNode(), value, comparison)) { return true; }
+                else if (contains(root.getLeftNode(), value, comparison)) { return true; }
+                return false;
+            }
+        }
+
+        protected override void InsertValue(T value, Comparison<T> comparison){
+            Node<T> newNode = new Node<T>(value);
+            root = root.addNodeElement(root, newNode, comparison);
+            size++;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        public bool isEmpty() {
+            if (size == 0) return true;
+            return false;
+        }
+
+        public void clearTree() {
+            size = 0;
+            root = null;
+        }
+    }
+}
