b792cfcedce1cc8290f315ffeb7a959d3138557a
victooorisimo <vmelendrez2013241@gmail.com>
2020-03-15 19:53:54 -0600

    Adding updates to branch

---
+++ HealthMedicine/Controllers/OrderController.cs
@@ -0,0 +1,96 @@
+﻿using HealthMedicine.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthMedicine.Controllers {
+    public class OrderController : Controller {
+        // GET: Order
+        public ActionResult Index() {
+            Order order = new Order();
+            return View(order.orders);
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection) {
+            try {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection) {
+            try {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection) {
+            try {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult LoadDocument(HttpPostedFileBase file){
+            string path = Server.MapPath($"~/Files/{file.FileName}");
+            try{
+                using (var fileStream = new FileStream(path, FileMode.Open)){
+                    using (var streamReader = new StreamReader(fileStream)){
+                        Medicine newMedicine = new Medicine();
+                        while (streamReader.Peek() >= 0){
+
+                        }
+                    }
+
+                }
+                    return RedirectToAction("Index");
+            }catch (Exception e) {
+                e.ToString();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+    }
+}
---
+++ HealthMedicine/Models/Medicine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HealthMedicine.Models {
+    public class Medicine {
+
+        public int idMedicine { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public String producer { get; set; }
+        public double price { get; set; }
+        public int stock { get; set; }
+
+        public Medicine() {
+
+        }
+
+        public static Comparison<Medicine> CompareByName = delegate (Medicine medicine_one, Medicine medicine_two) {
+            return medicine_one.name.ToLower().CompareTo(medicine_two.name.ToLower());
+        };
+
+    }
+}
